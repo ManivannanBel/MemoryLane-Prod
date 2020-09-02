f6fe0946a3cafe042d799afc2241d3abefe1ef9c
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-09-02 09:45:22 +0530

    2

---
+++ src/memory-lane-client/build/precache-manifest.afa064a88c0f389fbbc48940481118b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38eabcc3330697e77818e0c2c778bbee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55473fa7b64955336493",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f338ce3d801ee222b697",
+    "url": "/static/css/main.df379539.chunk.css"
+  },
+  {
+    "revision": "55473fa7b64955336493",
+    "url": "/static/js/2.ecce457e.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.ecce457e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f338ce3d801ee222b697",
+    "url": "/static/js/main.9cc4f6ae.chunk.js"
+  },
+  {
+    "revision": "b36baa1bf58edd9dc2b0",
+    "url": "/static/js/runtime-main.5a32d41d.js"
+  }
+]);
